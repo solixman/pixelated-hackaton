0b7ab3c4ea2371dd3492521687ec1ecca698dbb0
soulayman <soulaymanjaa09@gmail.com>
2025-03-24 12:20:37 +0100

    fixdorganisateur can set roles to users

--- app/Http/Controllers/HackatoneController.php
+++ app/Http/Controllers/HackatoneController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Hackatone;
 use Illuminate\Http\Request;
 
 class HackatoneController extends Controller
 {
-    //
+    public function create(Request $request){
+        $hachatone = new Hackatone();
+        $hachatone->title = $request['title'];
+        $hachatone->dateDebut = $request['dateDebut'];
+        $hachatone->dateFin = $request['dateFin'];
+        $hachatone->theme = $request['theme'];
+        $hachatone->regles = $request['regles'];
+        return response()->json('hackatone created succesfully');
+    }
+   
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Role;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     
  public function setRole(Request $request){
+    try{
     $user = User::find($request['id']);
-    $user->role_id=Role::where('name',$request['role']);
-    return response()->json('role set succesfully');
+    $role=Role::where('name',$request['role'])->first();
+    $user->role_id=$role->id;
+    $user->save();
+    return response()->json('role set succefully');
+    }catch(Exception $e){
+        return response()->json($e->getMessage());
+    }
  }    
 
 }
