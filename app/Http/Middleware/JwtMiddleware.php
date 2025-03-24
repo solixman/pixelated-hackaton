6033b0bbb6c8ab592c6b831c78a8b60f030994e8
soulayman <soulaymanjaa09@gmail.com>
2025-03-24 11:26:17 +0100

    JWTmilleware

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -3,20 +3,11 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-
-classuse Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
 class JwtMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
     public function handle($request, Closure $next)
     {
         try {
@@ -26,5 +17,5 @@ class JwtMiddleware
         }
 
         return $next($request);
- }
+    }
 }
