8a79779196810182dd0445b102b20389de2dc0bd
soulayman <soulaymanjaa09@gmail.com>
2025-03-24 11:59:46 +0100

    organisateur can set roles to users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    
+ public function setRole(Request $request){
+    $user = User::find($request['id']);
+    $user->role_id=Role::where('name',$request['role']);
+    return response()->json('role set succesfully');
+ }    
+
 }
